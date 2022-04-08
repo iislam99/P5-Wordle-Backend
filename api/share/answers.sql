2e04c34f93ffc62971b1b981fcc03251a962d8dd
AaronLieb <aaronlieberman@csu.fullerton.edu>
2022-04-08 06:20:12 +0000

    updated answer db to include queued answers

--- api/share/answers.sql
+++ api/share/answers.sql
@@ -4,7 +4,14 @@ PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS Answers;
 CREATE TABLE Answers (
-    id INT PRIMARY KEY,
-    word CHAR(5) UNIQUE 
+    id INTEGER,
+    word CHAR(5) UNIQUE,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS Queued_Answer;
+CREATE TABLE Queued_Answer (
+    word CHAR(5),
+    PRIMARY KEY(word)
 );
 COMMIT;
