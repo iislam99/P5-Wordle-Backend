54ecbe7d9662ca3e8277716ef8499a01e24fee79
Ubuntu <ubuntu@ip-172-31-16-80.ec2.internal>
2022-04-13 06:32:35 +0000

    began user.py

---
+++ api/user.py
@@ -0,0 +1,37 @@
+import collections
+import contextlib
+import sqlite3
+import typing
+
+from fastapi import FastAPI, Depends, Response, HTTPException, status
+from pydantic import BaseModel, BaseSettings
+
+class Settings(BaseSettings):
+    user_database: str
+    logging_config: str
+
+    class Config:
+        env_file = ".env"
+
+class User(BaseModel):
+    user_id: int
+    username: str
+
+class Game(BaseModel):
+    user_id: int
+    game_id: int
+    finished: datetime
+    guesses: int
+    won: bool
+
+def get_db():
+    with contextlib.closing(sqlite3.connect(settings.answers_database)) as db:
+        db.row_factory = sqlite3.Row
+        yield db
+
+
+@app.post("/end/", status_code=status.HTTP_200_OK)
+def process_end(
+    user_obj: User, response: Response, db: sqlite3.Connection = Depends(get_db)
+):
+    pass
