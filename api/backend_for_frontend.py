17d419e2ca431478496992527ac21f0986b7cdea
/home/student/.gitconfig <iislam99@csu.fullerton.edu>
2022-05-18 00:24:34 -0700

    File for new endpoints. Shell script for new game endpoint.

---
+++ api/backend_for_frontend.py
@@ -0,0 +1,20 @@
+import httpx
+from collections import OrderedDict
+from fastapi import FastAPI, Depends, Response, HTTPException, status
+from pydantic import BaseModel, BaseSettings
+
+class User(BaseModel):
+    username: str
+
+app = FastAPI()
+
+@app.post("/game/new/", status_code=status.HTTP_200_OK)
+def new_game(user: User, response: Response):
+    res = OrderedDict()
+    r = httpx.put('http://localhost:9999/start/', json={"username": user.username})
+    res.update(r.json())
+    return res
+
+@app.post("/game/{game_id}", status_code=status.HTTP_200_OK)
+def new_game(user: User, response: Response):
+    pass
